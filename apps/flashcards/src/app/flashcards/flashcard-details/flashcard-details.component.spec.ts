d7cb81165e585f0ed34d664fbc8518366db9a3d5
Victor Avila <vicav99@gmail.com>
2024-03-10 20:04:01 -0700

    update: flashcard unit tests passing

--- apps/flashcards/src/app/flashcards/flashcard-details/flashcard-details.component.spec.ts
+++ apps/flashcards/src/app/flashcards/flashcard-details/flashcard-details.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { mockFlashcard } from '@proto/testing';
 
 import { FlashcardDetailsComponent } from './flashcard-details.component';
 
@@ -8,12 +10,12 @@ describe('FlashcardDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FlashcardDetailsComponent]
-    })
-    .compileComponents();
-    
+      imports: [FlashcardDetailsComponent, NoopAnimationsModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(FlashcardDetailsComponent);
     component = fixture.componentInstance;
+    component.flashcard = mockFlashcard;
     fixture.detectChanges();
   });
 
