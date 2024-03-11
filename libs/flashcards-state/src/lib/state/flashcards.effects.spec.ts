1c11bc467757089b27d2db29cffdb405183e02c6
Victor Avila <vicav99@gmail.com>
2024-03-10 22:38:18 -0700

    update: effects unit tests passing

--- libs/flashcards-state/src/lib/state/flashcards.effects.spec.ts
+++ libs/flashcards-state/src/lib/state/flashcards.effects.spec.ts
@@ -1,39 +1,181 @@
-import { TestBed } from '@angular/core/testing';
-import { provideMockActions } from '@ngrx/effects/testing';
-import { Action } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
-import { hot } from 'jasmine-marbles';
-import { Observable } from 'rxjs';
+import { Flashcard } from '@proto/api-interfaces';
+import { FlashcardsService } from '@proto/flashcards-data';
+import { mockFlashcard, mockFlashcardsService } from '@proto/testing';
+import { of, throwError } from 'rxjs';
 
-import * as FlashcardsActions from './flashcards.actions';
-import { FlashcardsEffects } from './flashcards.effects';
+import { FlashcardsActions } from './flashcards.actions';
+import * as FlashcardsEffects from './flashcards.effects';
 
 describe('FlashcardsEffects', () => {
-  let actions: Observable<Action>;
-  let effects: FlashcardsEffects;
+  const service = mockFlashcardsService as unknown as FlashcardsService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [],
-      providers: [
-        FlashcardsEffects,
-        provideMockActions(() => actions),
-        provideMockStore(),
-      ],
+  describe('loadFlashcards$', () => {
+    it('should return loadFlashcardsSuccess, on success', (done) => {
+      const flashcards: Flashcard[] = [];
+      const action$ = of(FlashcardsActions.loadFlashcards());
+
+      service.all = jest.fn(() => of(flashcards));
+
+      FlashcardsEffects.loadFlashcards(action$, service).subscribe((action) => {
+        expect(action).toEqual(
+          FlashcardsActions.loadFlashcardsSuccess({ flashcards })
+        );
+        done();
+      });
     });
 
-    effects = TestBed.inject(FlashcardsEffects);
+    it('should return loadFlashcardsFailure, on failure', (done) => {
+      const error = new Error();
+      const action$ = of(FlashcardsActions.loadFlashcards());
+
+      service.all = jest.fn(() => throwError(() => error));
+
+      FlashcardsEffects.loadFlashcards(action$, service).subscribe((action) => {
+        expect(action).toEqual(
+          FlashcardsActions.loadFlashcardsFailure({ error })
+        );
+        done();
+      });
+    });
   });
 
-  describe('init$', () => {
-    it('should work', () => {
-      actions = hot('-a-|', { a: FlashcardsActions.initFlashcards() });
+  describe('loadFlashcard$', () => {
+    it('should return success with flashcard', (done) => {
+      const flashcard = { ...mockFlashcard };
+      const action$ = of(
+        FlashcardsActions.loadFlashcard({ flashcardId: flashcard.id as string })
+      );
 
-      const expected = hot('-a-|', {
-        a: FlashcardsActions.loadFlashcardsSuccess({ flashcards: [] }),
+      service.find = jest.fn(() => of(flashcard));
+
+      FlashcardsEffects.loadFlashcard(action$, service).subscribe((action) => {
+        expect(action).toEqual(
+          FlashcardsActions.loadFlashcardSuccess({ flashcard })
+        );
+        done();
       });
+    });
 
-      expect(effects.init$).toBeObservable(expected);
+    it('should return failure', (done) => {
+      const error = new Error();
+      const flashcard = { ...mockFlashcard };
+      const action$ = of(
+        FlashcardsActions.loadFlashcard({ flashcardId: flashcard.id as string })
+      );
+
+      service.find = jest.fn(() => throwError(() => error));
+
+      FlashcardsEffects.loadFlashcard(action$, service).subscribe((action) => {
+        expect(action).toEqual(
+          FlashcardsActions.loadFlashcardFailure({ error })
+        );
+        done();
+      });
+    });
+  });
+
+  describe('createFlashcard$', () => {
+    it('should return success with flashcard', (done) => {
+      const flashcard = { ...mockFlashcard };
+      const action$ = of(FlashcardsActions.createFlashcard({ flashcard }));
+
+      mockFlashcardsService.create = jest.fn(() => of(flashcard));
+
+      FlashcardsEffects.createFlashcard(action$, service).subscribe(
+        (action) => {
+          expect(action).toEqual(
+            FlashcardsActions.createFlashcardSuccess({ flashcard })
+          );
+          done();
+        }
+      );
+    });
+
+    it('should return failure', (done) => {
+      const error = new Error();
+      const flashcard = { ...mockFlashcard };
+      const action$ = of(FlashcardsActions.createFlashcard({ flashcard }));
+
+      service.create = jest.fn(() => throwError(() => error));
+
+      FlashcardsEffects.createFlashcard(action$, service).subscribe(
+        (action) => {
+          expect(action).toEqual(
+            FlashcardsActions.createFlashcardFailure({ error })
+          );
+          done();
+        }
+      );
+    });
+  });
+
+  describe('updateFlashcard$', () => {
+    it('should return success with flashcard', (done) => {
+      const flashcard = { ...mockFlashcard };
+      const action$ = of(FlashcardsActions.updateFlashcard({ flashcard }));
+
+      mockFlashcardsService.update = jest.fn(() => of(flashcard));
+
+      FlashcardsEffects.updateFlashcard(action$, service).subscribe(
+        (action) => {
+          expect(action).toEqual(
+            FlashcardsActions.updateFlashcardSuccess({ flashcard })
+          );
+          done();
+        }
+      );
+    });
+
+    it('should return failure', (done) => {
+      const error = new Error();
+      const flashcard = { ...mockFlashcard };
+      const action$ = of(FlashcardsActions.updateFlashcard({ flashcard }));
+
+      service.update = jest.fn(() => throwError(() => error));
+
+      FlashcardsEffects.updateFlashcard(action$, service).subscribe(
+        (action) => {
+          expect(action).toEqual(
+            FlashcardsActions.updateFlashcardFailure({ error })
+          );
+          done();
+        }
+      );
+    });
+  });
+
+  describe('deleteFlashcard$', () => {
+    it('should return success with flashcard', (done) => {
+      const flashcard = { ...mockFlashcard };
+      const action$ = of(FlashcardsActions.deleteFlashcard({ flashcard }));
+
+      mockFlashcardsService.delete = jest.fn(() => of(flashcard));
+
+      FlashcardsEffects.deleteFlashcard(action$, service).subscribe(
+        (action) => {
+          expect(action).toEqual(
+            FlashcardsActions.deleteFlashcardSuccess({ flashcard })
+          );
+          done();
+        }
+      );
+    });
+
+    it('should return failure', (done) => {
+      const error = new Error();
+      const flashcard = { ...mockFlashcard };
+      const action$ = of(FlashcardsActions.deleteFlashcard({ flashcard }));
+
+      service.delete = jest.fn(() => throwError(() => error));
+
+      FlashcardsEffects.deleteFlashcard(action$, service).subscribe(
+        (action) => {
+          expect(action).toEqual(
+            FlashcardsActions.deleteFlashcardFailure({ error })
+          );
+          done();
+        }
+      );
     });
   });
 });
--- libs/users-state/src/lib/state/users.effects.spec.ts
+++ libs/users-state/src/lib/state/users.effects.spec.ts
@@ -1,39 +1,145 @@
-import { TestBed } from '@angular/core/testing';
-import { provideMockActions } from '@ngrx/effects/testing';
-import { Action } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
-import { hot } from 'jasmine-marbles';
-import { Observable } from 'rxjs';
+import { User } from '@proto/api-interfaces';
+import { mockUser, mockUsersService } from '@proto/testing';
+import { UsersService } from '@proto/users-data';
+import { of, throwError } from 'rxjs';
 
-import * as UsersActions from './users.actions';
-import { UsersEffects } from './users.effects';
+import { UsersActions } from './users.actions';
+import * as UsersEffects from './users.effects';
 
 describe('UsersEffects', () => {
-  let actions: Observable<Action>;
-  let effects: UsersEffects;
+  const service = mockUsersService as unknown as UsersService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [],
-      providers: [
-        UsersEffects,
-        provideMockActions(() => actions),
-        provideMockStore(),
-      ],
+  describe('loadUsers$', () => {
+    it('should return loadUsersSuccess, on success', (done) => {
+      const users: User[] = [];
+      const action$ = of(UsersActions.loadUsers());
+
+      service.all = jest.fn(() => of(users));
+
+      UsersEffects.loadUsers(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.loadUsersSuccess({ users }));
+        done();
+      });
     });
 
-    effects = TestBed.inject(UsersEffects);
+    it('should return loadUsersFailure, on failure', (done) => {
+      const error = new Error();
+      const action$ = of(UsersActions.loadUsers());
+
+      service.all = jest.fn(() => throwError(() => error));
+
+      UsersEffects.loadUsers(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.loadUsersFailure({ error }));
+        done();
+      });
+    });
   });
 
-  describe('init$', () => {
-    it('should work', () => {
-      actions = hot('-a-|', { a: UsersActions.initUsers() });
+  describe('loadUser$', () => {
+    it('should return success with user', (done) => {
+      const user = { ...mockUser };
+      const action$ = of(UsersActions.loadUser({ userId: user.id as string }));
 
-      const expected = hot('-a-|', {
-        a: UsersActions.loadUsersSuccess({ users: [] }),
+      service.find = jest.fn(() => of(user));
+
+      UsersEffects.loadUser(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.loadUserSuccess({ user }));
+        done();
       });
+    });
 
-      expect(effects.init$).toBeObservable(expected);
+    it('should return failure', (done) => {
+      const error = new Error();
+      const user = { ...mockUser };
+      const action$ = of(UsersActions.loadUser({ userId: user.id as string }));
+
+      service.find = jest.fn(() => throwError(() => error));
+
+      UsersEffects.loadUser(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.loadUserFailure({ error }));
+        done();
+      });
+    });
+  });
+
+  describe('createUser$', () => {
+    it('should return success with user', (done) => {
+      const user = { ...mockUser };
+      const action$ = of(UsersActions.createUser({ user }));
+
+      mockUsersService.create = jest.fn(() => of(user));
+
+      UsersEffects.createUser(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.createUserSuccess({ user }));
+        done();
+      });
+    });
+
+    it('should return failure', (done) => {
+      const error = new Error();
+      const user = { ...mockUser };
+      const action$ = of(UsersActions.createUser({ user }));
+
+      service.create = jest.fn(() => throwError(() => error));
+
+      UsersEffects.createUser(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.createUserFailure({ error }));
+        done();
+      });
+    });
+  });
+
+  describe('updateUser$', () => {
+    it('should return success with user', (done) => {
+      const user = { ...mockUser };
+      const action$ = of(UsersActions.updateUser({ user }));
+
+      mockUsersService.update = jest.fn(() => of(user));
+
+      UsersEffects.updateUser(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.updateUserSuccess({ user }));
+        done();
+      });
+    });
+
+    it('should return failure', (done) => {
+      const error = new Error();
+      const user = { ...mockUser };
+      const action$ = of(UsersActions.updateUser({ user }));
+
+      service.update = jest.fn(() => throwError(() => error));
+
+      UsersEffects.updateUser(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.updateUserFailure({ error }));
+        done();
+      });
+    });
+  });
+
+  describe('deleteUser$', () => {
+    it('should return success with user', (done) => {
+      const user = { ...mockUser };
+      const action$ = of(UsersActions.deleteUser({ user }));
+
+      mockUsersService.delete = jest.fn(() => of(user));
+
+      UsersEffects.deleteUser(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.deleteUserSuccess({ user }));
+        done();
+      });
+    });
+
+    it('should return failure', (done) => {
+      const error = new Error();
+      const user = { ...mockUser };
+      const action$ = of(UsersActions.deleteUser({ user }));
+
+      service.delete = jest.fn(() => throwError(() => error));
+
+      UsersEffects.deleteUser(action$, service).subscribe((action) => {
+        expect(action).toEqual(UsersActions.deleteUserFailure({ error }));
+        done();
+      });
     });
   });
 });
