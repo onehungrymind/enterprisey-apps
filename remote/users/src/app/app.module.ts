7c228d0667570801e7dbc54f7f6590066f66ce3c
Luke Ruebbelke <lukas.ruebbelke@atlas.health>
2024-03-13 08:23:07 -0500

    update: auth stuff

--- remote/users/src/app/app.module.ts
+++ remote/users/src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [
+    forwardRef(() => UsersModule),
+    forwardRef(() => AuthModule),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
--- remote/users/src/app/users/users.module.ts
+++ remote/users/src/app/users/users.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { userProviders } from './user.providers';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [...userProviders, UsersService],
   exports: [UsersService],
