20c1bda64305a1337d8da61e2d1bb54f433a44ef
Luke Ruebbelke <lukas.ruebbelke@atlas.health>
2024-03-20 16:59:07 -0600

    update: remote definitions via features api

--- apps/dashboard/src/main.ts
+++ apps/dashboard/src/main.ts
@@ -1,6 +1,14 @@
 import { setRemoteDefinitions } from '@nx/angular/mf';
 
-fetch('/assets/module-federation.manifest.json')
+const FEATURES_API_URI = 'http://localhost:3000/api/features';
+
+fetch(FEATURES_API_URI)
   .then((res) => res.json())
-  .then((definitions) => setRemoteDefinitions(definitions))
+  .then((res) =>
+    res.reduce((acc: any, r: any) => {
+      acc[r.slug] = r.remote_uri;
+      return acc;
+    }, {})
+  )
+  .then((definitions: any) => setRemoteDefinitions(definitions))
   .then(() => import('./bootstrap').catch((err) => console.error(err)));
